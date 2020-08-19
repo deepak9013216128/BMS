2f3bc1ffbb798cd0e4c0b7015f580bcbaeaf45c8
deepak9013216128 <deepak9013216128@gmail.com>
2020-08-19 21:34:10 +0530

    create signup and login component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+
 import LandingPage from './pages/landing-page/landing-page.component';
+
+
+import SignUp from './components/signup/signup.component';
+import Login from './components/login/login.component';
 
 function App() {
   return (
     <div>
       <LandingPage />
+      <SignUp />
+      <Login />
     </div>
   );
 }
