24c2a5348c0584bb9fc5e61d10baa7612ba15cff
deepak9013216128 <deepak9013216128@gmail.com>
2020-09-03 18:44:29 +0530

    set up the redux for managing the state

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
+
+import rootReducer from './root-reducer';
+
+const middleware = [];
+if (process.env.NODE_ENV === 'development') {
+  middleware.push(logger)
+}
+export const store = createStore(rootReducer, applyMiddleware(...middleware))
+
+export const persistor = persistStore(store);
